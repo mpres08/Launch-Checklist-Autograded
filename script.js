ae537f0f1ddea233b1f334a793af671936028ab2
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-08-20 13:55:48 -0500

    not passing missionTarget test

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-// Write your JavaScript code here!
-
-// const { formSubmission } = require("./scriptHelper");
+const { pickPlanet, addDestinationInfo, formSubmission, myFetch } = require("./scriptHelper");
 
 window.addEventListener("load", function () {
-//   console.log("Started");
-//    let listedPlanets;
-//    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-//    let listedPlanetsResponse = planetsReturned;
-//    listedPlanetsResponse.then(function (result) {
-//        listedPlanets = result;
-//        console.log(listedPlanets);
-//    }).then(function () {
-//        console.log(listedPlanets);
-//        // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
-//    })
+  console.log("Started");
+   let listedPlanets;
+   let listedPlanetsResponse = planetsReturned;
+   listedPlanetsResponse.then(function (result) {
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
+       formSubmission();
+       myFetch();
+       pickPlanet();
+       addDestinationInfo();
+   })
 
   let form = this.document.querySelector('form');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-
     alert("form submitted");
 
-    // grab 4 inputs then pass into formSubmission
     let pilot = document.querySelector("input[name=pilotName]");
     let copilot = document.querySelector("input[name=copilotName]");
     let fuel = document.querySelector("input[name=fuelLevel]");
     let cargo = document.querySelector("input[name=cargoMass]");
-
-
-    //grab list 
     let list = document.getElementById("faultyItems");
 
     formSubmission(document, list, pilot, copilot, fuel, cargo);
-
   });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,27 +2,20 @@
 
 require("cross-fetch/polyfill");
 
-function addDestinationInfo(
-  document,
-  name,
-  diameter,
-  star,
-  distance,
-  moons,
-  imageUrl
-) {
-  // Here is the HTML formatting for our mission target div.
-  /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
+function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+  const missionTarget = document.getElementById("missionTarget");
+
+  missionTarget.innerHTML = `
+   <h2>Mission Destination</h2>
+      <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+      </ol>
+      <img src="${imageUrl}" alt="Image of ${name}">
+  `; 
 }
 
 function validateInput(testInput) {
@@ -98,12 +91,17 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
   let planetsReturned;
 
-  planetsReturned = await fetch().then(function (response) {});
+  planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+    return response.json();
+  });
 
   return planetsReturned;
 }
 
-function pickPlanet(planets) {}
+function pickPlanet(planets) {
+  const randomIndex = Math.floor(Math.random() * planets.length);
+  return planets[randomIndex];
+}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
